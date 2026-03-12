61fe88dc96b35c9c4fd244a02b9a4e2b4377badc
Gauravlovescode <gqurav69@gmail.com>
2026-03-12 09:10:48 +0545

    Enhance GUI design by adding place selection and submit button

--- week_3/design.java
+++ week_3/design.java
@@ -2,10 +2,13 @@
 import java.awt.Color;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 
@@ -28,20 +31,35 @@ public class design {
         emailLabel.setBounds(50, 100, 100, 30);
         JLabel gender = new JLabel("Gender ");
         gender.setBounds(50, 150, 100, 30);
+        JLabel placeLabel = new JLabel("Place");
+        placeLabel.setBounds(50, 190, 100, 30);
+        panel.add(placeLabel);
 
         JTextField emailField = new JTextField();
         emailField.setBounds(150, 110, 200, 30);
-        JTextField genderField = new JTextField();
 
         JRadioButton male = new JRadioButton("Male");
         JRadioButton female = new JRadioButton("Female");
+        male.setBounds(150, 150, 80, 30);
+        female.setBounds(250, 150, 80, 30);
 
         ButtonGroup group = new ButtonGroup();
         group.add(male);
         group.add(female);
 
+        String[] places = { "Itahari", "Damak", "Biratnagar", "Dharan" };
+        JComboBox<String> placeComboBox = new JComboBox<>(places);
+        placeComboBox.setBounds(150, 190, 200, 30);
+
+        JTextArea text = new JTextArea();
+        text.setBounds(180, 240, 120, 30);
+        JButton submit = new JButton("Submit");
+        submit.setBounds(180, 290, 120, 30);
+
+        panel.add(text);
+        panel.add(submit);
+        panel.add(placeComboBox);
         panel.add(gender);
-        panel.add(genderField);
         panel.add(male);
         panel.add(female);
         panel.add(emailField);
