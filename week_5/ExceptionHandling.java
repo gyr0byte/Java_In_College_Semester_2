a36149f1e99b03f54f8d5c396d9b5135090f176e
Gauravlovescode <gqurav69@gmail.com>
2026-04-02 08:49:29 +0545

    Refactor ExceptionHandling class to include try-catch for input validation

--- week_5/ExceptionHandling.java
+++ week_5/ExceptionHandling.java
@@ -1,13 +1,20 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionHandling {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int num1 = sc.nextInt();
-        System.out.println("Enter another number: ");
-        int num2 = sc.nextInt();
-        System.out.println("The division is: " + (num1/num2));
-        sc.close();
+        try {
+            System.out.print("Enter a number: ");
+            int num1 = sc.nextInt();
+            System.out.print("Enter another number: ");
+            int num2 = sc.nextInt();
+            System.out.println("The division is: " + (num1 / num2));
+            sc.close();
+        } 
+        catch (InputMismatchException e) {
+
+        }
+        System.out.println("Hello World!");
     }
 }
